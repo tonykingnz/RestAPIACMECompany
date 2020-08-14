8b43f7eb58d9838e1a9caea3cd4a860f7706fd34
{tonyKingNZ, santiagoalves} <noreply@users.noreply.github.com>
2020-08-14 20:02:54 +0000

    Create Slipt Paramter

--- testOrder/testOrder.py
+++ testOrder/testOrder.py
@@ -17,6 +17,20 @@ from app import paymentInformation
 from app import refund
 from app import refundItem
 
+def stripIgnoreProperties(dictionary={}, ignoreProperties=[], inside=""):
+    it = 0
+    for element in dictionary[inside]:
+        for ignore in ignoreProperties:
+            del element[ignore]
+            dictionary[inside][it] = element
+        it += 1
+    return dictionary
+
+def stripIgnorePropertiesWithoutInside(dictionary={}, ignoreProperties=[]):
+    for ignore in ignoreProperties:
+        del dictionary[ignore]
+    return dictionary
+
 class TestOrder(unittest.TestCase):
     def test01CreateOrder(self):
         print("")
@@ -30,21 +44,25 @@ class TestOrder(unittest.TestCase):
         with open('updateOrderTestCase.json') as payloadFile:
             payload = json.load(payloadFile)
             for testCase in payload:
-                self.assertEqual(updateOrder(testCase['input']['address'], testCase['input']['id'])[1], testCase['output'], "Update order failed!")
                 print("")
+                self.assertEqual(updateOrder(testCase['input']['address'], testCase['input']['id'])[1], testCase['output'], "Update order failed!")
 
     def test03ListOrder(self):
         print("")
         with open('listOrderTestCase.json') as payloadFile:
             payload = json.load(payloadFile)
             for testCase in payload:
-                self.assertEqual(listOrder(testCase['input']), testCase['output'], "List order failed!")
+                ignoreProperties = ["confirmationDate"]
+                inside = "orders"
+                actual = stripIgnoreProperties(listOrder(testCase['input']), ignoreProperties, inside)
+                self.assertEqual(actual, testCase['output'], "List order failed!")
 
     def test04DetailOrder(self):
         print("")
         with open('detailOrderTestCase.json') as payloadFile:
             payload = json.load(payloadFile)
             for testCase in payload:
+                ignoreProperties = ["confirmationDate"]
                 self.assertEqual(str(detailOrder(testCase['input'])), str(testCase['output']), "Detail order failed!")
 
     def test05CreatePaymentOrder(self):
@@ -52,15 +70,16 @@ class TestOrder(unittest.TestCase):
         with open('createPaymenteOrderTestCase.json') as payloadFile:
             payload = json.load(payloadFile)
             for testCase in payload:
-                self.assertEqual(str(createPayment(testCase["input"]['id'], testCase["input"])),str(testCase["output"]), "Order refund failed!")
+                self.assertEqual(str(createPayment(testCase["input"]['id'], testCase["input"])),str(testCase["output"]), "Create payment failed")
 
     def test06InfoPaymentOrder(self):
         print("")
         with open('informationPaymenteOrderTestCase.json') as payloadFile:
             payload = json.load(payloadFile)
             for testCase in payload:
-                self.assertEqual(str(paymentInformation(testCase["input"])),str(testCase["output"]), "Order refund failed!")
-
+                ignoreProperties = ["paymentDate"]
+                actual = stripIgnorePropertiesWithoutInside(paymentInformation(testCase['input']), ignoreProperties)
+                self.assertEqual(str(actual), str(testCase['output']), "Info about payment failed!")
     def test07OrderRefund(self):
         print("")
         with open('orderRefundTestCase.json') as payloadFile:
